3a35d8dcfe7d7f736c7fa7e34f8e27070843b7e1
anna-sakhlyan <anna.sakhlyan@picsart.com>
2017-08-07 01:38:29 +0400

    Initial Commit

---
+++ FirebaseTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirebaseTest
+//
+//  Created by AnnaSakhlyan on 8/7/17.
+//  Copyright © 2017 AnnaSakhlyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirebaseTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FirebaseTest
+//
+//  Created by AnnaSakhlyan on 8/7/17.
+//  Copyright © 2017 AnnaSakhlyan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
